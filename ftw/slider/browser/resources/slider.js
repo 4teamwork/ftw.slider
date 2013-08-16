b358ef091f33be3ceaf520bbf33f17a02dc94e17
Julian Infanger <j.infanger@4teamwork.ch>
2013-08-16 14:24:25 +0200

    Updated css, added a javascript to adjust the image sizes for responsive devices.

--- ftw/slider/browser/resources/slider.js
+++ ftw/slider/browser/resources/slider.js
@@ -1,4 +1,16 @@
+function adjust_slider_size() {
+  $('#slider-wrapper, #slider-wrapper #slider-panes div.sliderPane').css(
+    'width',
+    $('#content').innerWidth()+'px');
+}
+
+
 jQuery(function($) {
+  adjust_slider_size();
+  $(window).resize(function() {
+    adjust_slider_size();
+  });
+
   var root = $("#slider-wrapper").scrollable({
     circular: true
   }).autoscroll({
