c5bcf43f4075fdccab5de77b100a153008bec6d7
Kevin Bieri <k.bieri@4teamwork.ch>
2015-06-30 15:17:56 +0200

    Responsive behaviour is done by update of slick.js

--- ftw/slider/browser/resources/slider.js
+++ ftw/slider/browser/resources/slider.js
@@ -1,17 +1,6 @@
-function adjust_slider_size() {
-  $('#slider-panes, #slider-wrapper #slider-panes div.sliderPane').css(
-    'width',
-    $('#content').innerWidth()+'px');
-}
-
 jQuery(function($) {
   $('.enabled #slider-panes').slick({
     fade: true,
     autoplay: true
-  });
-
-  adjust_slider_size();
-  $(window).resize(function() {
-    adjust_slider_size();
   });
 });
