3baed8689f3bf7e7be32778849962491780f1d22
Kevin Bieri <k.bieri@4teamwork.ch>
2015-06-30 17:14:21 +0200

    Implement play and pause button.
    Use DOM exposed configuratino for slider initialisation.
    Extend configuration for play and pause functionality.

--- ftw/slider/browser/resources/slider.js
+++ ftw/slider/browser/resources/slider.js
@@ -1,6 +1,49 @@
-jQuery(function($) {
-  $('.enabled #slider-panes').slick({
-    fade: true,
-    autoplay: true
+(function() {
+
+  "use strict";
+
+  var Slider =  function(element, config) {
+
+    config = $.extend({
+      canPlay: false,
+      canPause: false
+    }, config);
+
+    var buttonTemplate = '<button type="button" class="slick-{{:action}}" aria-label="{{:action}}">{{:action}}</button>'
+
+    var init = function() {
+      $(element).slick(config);
+      if(config.canPlay) {
+        addPlayButton();
+      }
+      if(config.canPause) {
+        addPauseButton();
+      }
+    };
+
+    var play = function() { element.slick("slickPlay"); };
+
+    var pause = function() { element.slick("slickPause"); };
+
+    var addPlayButton = function() {
+      var button = $(buttonTemplate.replace(/{{:action}}/g, "play"));
+      button.on("click", play);
+      element.append(button);
+    }
+
+    var addPauseButton = function() {
+      var button = $(buttonTemplate.replace(/{{:action}}/g, "pause"));
+      button.on("click", pause);
+      element.append(button);
+    }
+
+    init();
+  }
+
+  $(function() {
+    $(".sliderWrapper").each(function() {
+      var slider = new Slider($(".sliderPanes", this), JSON.parse(this.dataset.settings));
+    });
   });
-});
+
+})();
