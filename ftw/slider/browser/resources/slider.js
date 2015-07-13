2a0cb33d48210a3057a4531ca9fa62c7c1651867
Kevin Bieri <k.bieri@4teamwork.ch>
2015-07-13 13:54:19 +0200

    Implement update function.
    Due to https://github.com/4teamwork/bern.web/issues/204 the slider
    cannot get initialized twice. So an update function is required to not
    adding the play and pause buttons twice.

--- ftw/slider/browser/resources/slider.js
+++ ftw/slider/browser/resources/slider.js
@@ -60,8 +60,16 @@
         var slider = new Slider($(".sliderPanes", this), $(this).data("settings"));
       });
     };
+    var ftwSliderUpdate = function() {
+      $(".sliderWrapper").each(function() {
+        if($("> div.slick-initialized", this).length < 1) {
+          var slider = new Slider($(".sliderPanes", this), $(this).data("settings"));
+        }
+      });
+    };
     ftwSliderInit();
     global.ftwSliderInit = ftwSliderInit;
+    global.ftwSliderUpdate = ftwSliderUpdate;
   });
 
 })(window);
