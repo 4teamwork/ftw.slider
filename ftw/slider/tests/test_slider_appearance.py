c41fc35fd7144a55802e80942d2eff4bb6504f1b
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-08-10 11:50:23 +0200

    Add css class if pane has text

--- ftw/slider/tests/test_slider_appearance.py
+++ ftw/slider/tests/test_slider_appearance.py
@@ -2,10 +2,11 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.slider.testing import SLIDER_FUNCTIONAL_TESTING
 from ftw.testbrowser import browsing
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import login
 from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.textfield.value import RichTextValue
 from unittest2 import TestCase
 
 
@@ -22,12 +23,12 @@ class TestSliderCreation(TestCase):
         self.folder = create(Builder('folder')
                              .titled(u'Folder'))
 
-        container = create(Builder('slider container')
-                           .within(self.folder)
-                           .titled(u'Slider Container'))
+        self.container = create(Builder('slider container')
+                                .within(self.folder)
+                                .titled(u'Slider Container'))
 
         create(Builder('slider pane')
-               .within(container)
+               .within(self.container)
                .titled(u'Pane 1')
                .with_dummy_image())
 
@@ -62,3 +63,18 @@ class TestSliderCreation(TestCase):
 
         self.assertEqual(1, len(browser.css('body.portaltype-document')))
         self.assertEqual(1, len(browser.css('#slider-wrapper')))
+
+    @browsing
+    def test_slider_has_special_css_class_if_text_is_available(self, browser):
+        browser.login().visit(self.folder)
+        self.assertEqual(0, len(browser.css('sliderPaneHasText')))
+
+        create(Builder('slider pane')
+               .within(self.container)
+               .titled(u'Pane 2')
+               .having(text=RichTextValue("Test text"))
+               .with_dummy_image())
+
+        browser.visit(self.folder)
+
+        self.assertEqual(1, len(browser.css('.sliderPaneHasText')))
