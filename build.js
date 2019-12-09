9662427c708e715c554c865059fdf9c91627844c
Thomas Buchberger <t.buchberger@4teamwork.ch>
2019-12-09 08:43:41 +0100

    Provide precompiled bundle

---
+++ build.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./ftw/slider/browser/resources",
+    paths: {
+        jquery: "empty:",
+    },
+    name: "main",
+    out: "./ftw/slider/browser/resources/slider-compiled.js"
+})
---
+++ ftw/slider/browser/resources/main.js
@@ -0,0 +1,1 @@
+require(['slick', 'slider'], function (slick, slider) {});
